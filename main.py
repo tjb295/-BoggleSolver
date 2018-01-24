7500e27306ade2c89ec3db264b0fede1d819766d
Thomas Back <tjb295@nau.edu>
2018-01-24 16:39:48 -0700

    Ran the code and it was terrible (haven't pythoned in years) cleaning up a lot of syntax, mainly forget you need to specify enumuration for for loops focused on indexing

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ class boggleSolver:
         self.board = []
 
     #loads NxN board into matrix
-    def loadBoard(boardFile):
+    def loadBoard(self, boardFile):
         #temp array
         temp = []
         count = 0
-        
+
         board = open(boardFile)
-        board.strip(" ")
+        board = board.read()
+        board.strip(' ')
 
         #for loop to loop through letters, disregards white space
-        for letter in board:
+        for letter in range(len(board)):
 
             #if reached new line, save that dimension within array
             if board[letter] == '\n':
@@ -34,30 +35,36 @@ class boggleSolver:
 
             else :
                 temp[count] = board[letter]
-                count ++
+                count += 1
             
         
         
         return self.board
 
-    def printBoard(board):
-        for col in board:
-            for row in board[col]:
-                print board[col][row]
-                print " "
-            print"\n"
+    def printBoard(self,board):
+        
+        for col in range(len(board)):
+            
+            for row in range(len(board[col])):
+                print(board[col][row])
+                print(" ")
+                
+            print("\n")
                 
 
-    def possibleMoves():
+    def possibleMoves(self):
+        return []
 
-    def legalMoves():
+    def legalMoves(self):
+        return []
 
-    def examineState():
+    def examineState(self):
+        return []
 
 
 def main():
     solve = boggleSolver()
-    myboard = solve.loadBoard("board.txt")
+    myboard = solve.loadBoard('boardex')
     solve.printBoard(myboard)
     
 
