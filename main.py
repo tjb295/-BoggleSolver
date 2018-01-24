97424daf0d1ec5fa99fe1569d77333e897e9229d
Thomas Back <tjb295@nau.edu>
2018-01-24 15:23:07 -0700

    Defined the functions required for part1

---
+++ main.py
@@ -0,0 +1,21 @@
+#### Boggle Solver Main ####
+#### By Thomas Back     ####
+#### 1/24/2018 CS470    ####
+
+import time
+
+class boggleSolver:
+    
+    def __init__(self):
+        
+    def loadBoard(boardFile):
+        return []
+
+    def printBoard():
+
+    def possibleMoves():
+
+    def legalMoves():
+
+    def examineState():
+        
